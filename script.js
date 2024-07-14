31065aa22820bde72cdd97cf0572144e54af69ab
lunatrifx <pnickqlim@gmail.com>
2024-07-14 14:26:41 -0700

    easter egg included

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ document.getElementById("generate-button").addEventListener("click", function() 
     const inputText = document.getElementById("input-text").value;
     const outputText = generateMeme(inputText);
     document.getElementById("output-text").value = outputText;
-    changeImages();
+    if (outputText.includes("kissmaxxing")) {
+        fadeToKissmaxxing();
+    } else {
+        changeImages();
+    }
 });
 
 function generateMeme(phrase) {
@@ -15,8 +19,14 @@ function changeImages() {
     const imageLeft = document.getElementById("image-left");
     const imageRight = document.getElementById("image-right");
 
-    const timestamp = new Date().getTime();
+    imageLeft.src = "image3.jpg";
+    imageRight.src = "image4.jpg";
+}
 
-    imageLeft.src = `image3.jpg?${timestamp}`;
-    imageRight.src = `image4.jpg?${timestamp}`;
+function fadeToKissmaxxing() {
+    const fadeImage = document.getElementById("image-fade");
+    fadeImage.style.display = "block";
+    setTimeout(() => {
+        fadeImage.style.opacity = "1";
+    }, 10);
 }
